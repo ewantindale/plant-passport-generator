1acb6928e391291ce395eb0705157f5d5b2d0eba
Ewan Tindale <ewan.tindale@datacap.co.uk>
2020-12-29 10:44:59 +0000

    Brexit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import eu from "./images/eu.png";
 function App() {
   const [a, setA] = useState("");
   const [b, setB] = useState(localStorage.getItem("plant_passport_b") || "");
-  const [c, setC] = useState("");
-  const [d, setD] = useState("");
+  const [c, setC] = useState("130975");
+  const [d, setD] = useState("UK");
 
   const generatePDF = (e) => {
     e.preventDefault();
@@ -25,11 +25,11 @@ function App() {
     for (let x = 0; x < 2; x += 1) {
       for (let y = 0; y < 8; y += 1) {
         // add EU flag
-        doc.addImage(eu, "PNG", x * TILE_WIDTH, y * TILE_HEIGHT, 69, 45);
+        // doc.addImage(eu, "PNG", x * TILE_WIDTH, y * TILE_HEIGHT, 69, 45);
 
         // draw title and letters in bold
         doc.setFont("Times", "bold");
-        doc.text("Plant Passport", x * TILE_WIDTH + 175, y * TILE_HEIGHT + 20);
+        doc.text("UK Plant Passport", x * TILE_WIDTH + 175, y * TILE_HEIGHT + 20);
         doc.text("A ", x * TILE_WIDTH + 10, y * TILE_HEIGHT + 60);
         doc.text("B ", x * TILE_WIDTH + 10, y * TILE_HEIGHT + 80);
         doc.text("C ", x * TILE_WIDTH + 210, y * TILE_HEIGHT + 60);
@@ -53,38 +53,45 @@ function App() {
       <h4>Enter the values you want and then click Generate PDF.</h4>
 
       <form onSubmit={generatePDF}>
-        <div>
-          <label htmlFor="a">A </label>
-          <input
-            type="text"
-            name="a"
-            value={a}
-            onChange={(e) => setA(e.target.value)}
-          />
-
-          <label htmlFor="c">C </label>
-          <input
-            type="text"
-            name="c"
-            value={c}
-            onChange={(e) => setC(e.target.value)}
-          />
+        <div className="inputRow">
+          <div className="inputField">
+            <label htmlFor="a">A </label>
+            <input
+              type="text"
+              name="a"
+              value={a}
+              onChange={(e) => setA(e.target.value)}
+            />
+          </div>
+          <div className="inputField">
+            <label htmlFor="c">C </label>
+            <input
+              type="text"
+              name="c"
+              value={c}
+              onChange={(e) => setC(e.target.value)}
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="b">B </label>
-          <input
-            type="text"
-            name="b"
-            value={b}
-            onChange={(e) => setB(e.target.value)}
-          />
-          <label htmlFor="d">D </label>
-          <input
-            type="text"
-            name="d"
-            value={d}
-            onChange={(e) => setD(e.target.value)}
-          />
+        <div className="inputRow">
+          <div className="inputField">
+            <label htmlFor="b">B </label>
+            <input
+              type="text"
+              name="b"
+              value={b}
+              onChange={(e) => setB(e.target.value)}
+            />
+          </div>
+          <div className="inputField">
+            <label htmlFor="d">D </label>
+            <input
+              type="text"
+              name="d"
+              value={d}
+              onChange={(e) => setD(e.target.value)}
+            />
+          </div>
         </div>
         <div>
           <button type="submit" className="generateButton">
@@ -92,14 +99,27 @@ function App() {
           </button>
         </div>
       </form>
-      <a
-        href="https://planthealthportal.defra.gov.uk/assets/uploads/Plant-Passport-Introductory-Guide.pdf"
-        style={{ marginTop: "5rem" }}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Plant Passport Introductory Guide
-      </a>
+      <span className="guideLink">
+        Need help? Read the{" "}
+        <a
+          href="https://planthealthportal.defra.gov.uk/assets/uploads/Plant-Passport-Introductory-Guide.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Plant Passport Introductory Guide
+        </a>
+      </span>
+
+      <div className="footer">
+        Created by{" "}
+        <a
+          href="https://www.ewantindale.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ewan Tindale
+        </a>
+      </div>
     </div>
   );
 }
